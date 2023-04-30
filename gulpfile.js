c8c3d401e5868a3ee511bb22d46d36fbff77eabf
Nash-Aud <92648695+Nash-Aud@users.noreply.github.com>
2023-04-30 11:19:05 -0400

    Project finished - La Cafeteria

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,15 @@ const { src, dest, watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const cssnano = require('cssnano');
 
 function css( done ) {
-    src('src/scss/app.scss')        
+    src('src/scss/app.scss')
+        .pipe( sourcemaps.init() )        
         .pipe( sass() )
-        .pipe( postcss([ autoprefixer() ]) )
+        .pipe( postcss([ autoprefixer(), cssnano() ]) )
+        .pipe( sourcemaps.write('.'))
         .pipe( dest('build/css') )
 
     done();
